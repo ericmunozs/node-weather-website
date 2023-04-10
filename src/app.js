c6a51d77db35b7edebe122fbff08f72f14758f6c
Eric <eric.munozsoldado@gmail.com>
2023-04-10 12:47:37 +0200

    start script added

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-console.log(__dirname)
-console.log(path.join(__dirname, '../public'))
+// console.log(__dirname)
+// console.log(path.join(__dirname, '../public'))
 
 const app = express()
 
